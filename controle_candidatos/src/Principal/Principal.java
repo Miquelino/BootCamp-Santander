15e6dc5dda30fcc8c464817582c945123f67de7d
Miquelino <reynaldomiquelinoneto@hotmail.com>
2025-06-24 19:49:33 -0300

    Iniciando os exercicios

--- controle_candidatos/src/Principal/Principal.java
+++ controle_candidatos/src/Principal/Principal.java
@@ -1,86 +1,41 @@
-package candidatura;
+package Principal;
 
-import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
+import Exception.ParametrosInvalidosException;
+import java.util.Scanner;
 
-public class processoSeletivo {
+public class Principal {
 
-    public processoSeletivo() {
+    Scanner leitura = new Scanner(System.in);
+
+    public Principal() {
     }
 
     public void exibirMenu() {
-        imprimirSelecionados();
-    }
+        System.out.println("Digite o primeiro número: ");
+        var numeroUm = leitura.nextInt();
+        System.out.println("Digite o segundo número: ");
+        var numeroDois = leitura.nextInt();
 
-    public void analisarCandidato(double salarioPretendido) {
-        double salarioBase = 2000.0;
-        if (salarioBase > salarioPretendido) {
-            System.out.println("Ligar para o candidato");
-        } else if (salarioBase == salarioPretendido) {
-            System.out.println("Ligar para o candidato com contra porposta");
-        } else {
-            System.out.println("Aguardando o resultado dos demais candidatos");
+        try {
+            contar(numeroUm, numeroDois);
+        } catch (ParametrosInvalidosException e) {
+            System.err.println("Erro: " + e.getMessage());
         }
     }
 
-    public void imprimirSelecionados() {
-        String[] candidatos = {"Felipe", "Marcia", "Andre", "Ariane", "Joao", "Ana", "Fabio", "Julia", "Carlos", "Bea"};
-        for (int i = 0; i < candidatos.length; i++) {
-            System.out.println("O candidato de n " + (i + 1) + " é o " + candidatos[i]);
+    static void contar(int numeroUm, int numeroDois) {
+        if (numeroUm >= numeroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
         }
 
-        System.out.println("Forma abreviada");
-        for (String candidato : candidatos) {
-            System.out.println("O candidato selecioando foi: " + candidato);
+        int contagem = numeroDois - numeroUm;
+
+        System.out.println("\nIniciando contagem:");
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + i);
         }
+        System.out.println("\nContagem finalizada. Total de interações: " + contagem);
+
     }
-
-    public void selecaoCandidatos() {
-        String[] candidatos = {"Felipe", "Marcia", "Andre", "Ariane", "Joao", "Ana", "Fabio", "Julia", "Carlos", "Bea"};
-
-        int candidatosSelecionados = 0;
-        int candidatosAtual = 0;
-        double salarioBase = 2000.0;
-        while (candidatosSelecionados < 5 && candidatosAtual < candidatos.length) {
-            String candidato = candidatos[candidatosAtual];
-            double salarioPretendido = valorPretendido();
-
-            System.out.println("O candidato " + candidato + " solicitou este valor de salario " + salarioPretendido);
-            if (salarioBase >= salarioPretendido) {
-                System.out.println("O candidato  " + candidato + " foi selecionado para vaga");
-                candidatosSelecionados++;
-            }
-            candidatosAtual++;
-        }
-    }
-
-    static double valorPretendido() {
-        return ThreadLocalRandom.current().nextDouble(1800, 2200);
-    }
-
-    static boolean atender() {
-        return new Random().nextInt(3) == 1;
-    }
-
-    static void entrandoEmCotato(String candidato) {
-        int tentativasRealizadas = 1;
-        boolean continuarTentando = true;
-        boolean atendeu = false;
-        do {
-            atendeu = atender();
-            continuarTentando = !atendeu;
-            if (continuarTentando) {
-                tentativasRealizadas++;
-            } else {
-                System.out.println("Contato realizado com sucesso!");
-            }
-        } while (continuarTentando && tentativasRealizadas < 3);
-
-        if (atendeu) {
-            System.out.println("Conseguimos contato com " + candidato);
-        } else {
-            System.out.println("Não conseguimos cobtato");
-        }
-    }
-
 }
+
