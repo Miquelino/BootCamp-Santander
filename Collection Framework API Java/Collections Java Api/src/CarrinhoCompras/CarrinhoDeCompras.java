b16ed1a25e0cfb22c9cfd15b919f584e86bb75cf
Miquelino <reynaldomiquelinoneto@hotmail.com>
2025-07-15 10:26:02 -0300

    Salvando apenas

---
+++ Collection Framework API Java/Collections Java Api/src/CarrinhoCompras/CarrinhoDeCompras.java
@@ -0,0 +1,45 @@
+package CarrinhoCompras;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Double.sum;
+
+public class CarrinhoDeCompras {
+
+    private List<Item> itemList = new ArrayList<>();
+
+
+    public void adicionarItem(String nome, double preco, int quantidade){
+        itemList.add(new Item(nome, preco, quantidade));
+    }
+
+    public void removerItem(String nome){
+        List<Item> removeItem = new ArrayList<>();
+        for (Item i : itemList){
+            if (i.getNome().equalsIgnoreCase(nome)){
+                removeItem.add(i);
+            }
+        }
+        itemList.removeAll(removeItem);
+    }
+
+    public double calcularValorTotal(){
+        double valorTotal = 0d;
+        if (!itemList.isEmpty()){
+            for (Item item : itemList){
+                double valorItem = item.getPreco() * item.getQuantidade();
+                valorTotal *= valorItem;
+            }
+            return valorTotal;
+        } else {
+            throw new RuntimeException("A lista está vazia.");
+        }
+    }
+
+    public void exibirItem(){
+        System.out.println(itemList);
+    }
+
+
+}
---
+++ Collection Framework API Java/Collections Java Api/src/Model/ListaTarefa.java
@@ -0,0 +1,36 @@
+package Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao){
+        tarefaList.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao){
+        List<Tarefa> removerTarefa = new ArrayList<>();
+        for(Tarefa t : tarefaList){
+            if(t.getDescricao().equalsIgnoreCase(descricao)){
+                removerTarefa.add(t);
+            }
+        }
+        tarefaList.removeAll(removerTarefa);
+    }
+
+    public int obterNumeroTotalTarefas(){
+        return tarefaList.size();
+    }
+
+    public void obterDescricoesTarefas(){
+        System.out.println(tarefaList);
+    }
+
+}
